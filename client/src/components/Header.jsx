fea0d851120dc46a3256611c6541d0adc783c7a2
Terrence Rosales <118107221+Romo3@users.noreply.github.com>
2024-06-26 09:03:46 -0400

    set up header and api server

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import "../global.css";
+
+function Header() {
+  return (
+    <div className="bg-slate-50 shadow">
+      <div className="max-w-6xl flex items-center justify-between mx-auto p-3">
+        <Link to="/">
+          <img
+            className="w-[100px] h-[95px]"
+            src="/thai-rest-brand.png"
+            alt="thai rest logo"
+          />
+        </Link>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/controls">
+            <li>Controls</li>
+          </Link>
+          <Link to="/siginin">
+            <li>Sigin In</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
