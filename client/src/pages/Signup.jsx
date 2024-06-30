29393a9fe7a6e504309d6e2c14a269dffc938353
Terrence Rosales <118107221+Romo3@users.noreply.github.com>
2024-06-30 14:58:48 -0400

    signup functionallity finished

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,23 +1,47 @@
 import React, { useState } from "react";
 import { TiInfo } from "react-icons/ti";
 import { Link } from "react-router-dom";
+import { Button, Alert } from "flowbite-react";
 import "../global.css";
 
 function Signup() {
   const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
   const handleChange = (e) => {
-    e.preventDefault();
     setFormData({ ...formData, [e.target.id]: e.target.value });
-    console.log(formData);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
   };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="pageTitle text-center  my-7">Sign Up</h1>
-      <form className="flex flex-col gap-5">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
         <input
           type="text"
           placeholder="Username"
-          id="Username"
+          id="username"
           className="bg-slate-100 p-3 rounded-lg"
           onChange={handleChange}
         />
@@ -35,9 +59,13 @@ function Signup() {
           className="bg-slate-100 p-3 rounded-lg"
           onChange={handleChange}
         />
-        <button className="bg-red-400 text-white p-2 rounded-lg w-[200px] self-center">
-          Sign Up
-        </button>
+        <Button
+          type="submit"
+          gradientDuoTone="pinkToOrange"
+          className="bg-red-400 text-white rounded-lg w-[200px] self-center"
+        >
+          {loading ? "Loading..." : "Sign Up"}
+        </Button>
       </form>
       <div className="flex my-4">
         <p>Have an account?&nbsp;</p>
@@ -46,19 +74,22 @@ function Signup() {
         </Link>
       </div>
       <section className="gradient p-4 rounded-2xl  shadow flex flex-col my-5 items-center">
+        {error && (
+          <div className="flex">
+            <TiInfo className="text-3xl text-yellow-300 bg-black rounded-lg" />
+            <Alert color="red" className="text-black text-md font-semibold p-1">
+              Something Went Wrong, Please Contact Your Manager
+            </Alert>
+          </div>
+        )}
         <div className="flex">
-          <TiInfo className="text-3xl text-yellow-300" />
-          <p
-            className="
-          onChange={handleChange}font-semibold  p-1"
-          >
-            Company Personels Only
-          </p>
+          <TiInfo className="text-3xl text-red-500" />
+          <p className="font-semibold  p-1">Company Personels Only</p>
         </div>
         <div className="flex">
-          <TiInfo className="text-3xl text-yellow-300" />
+          <TiInfo className="text-3xl text-red-500" />
           <p className="font-semibold  p-1">
-            Contact Manager for Authorization Step
+            Contact Manager for Authorization Steps
           </p>
         </div>
       </section>
